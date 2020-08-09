2a9d1fe6838bb52cfcc1071bdb389511725c205e
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-08-09 10:38:49 -0700

    Add project files.

---
+++ EnterpriseApp1.Mobile/EnterpriseApp1.Mobile/Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace EnterpriseApp1.Mobile.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() => InitializeComponent();
+    }
+}
